8471a5b70708e724e0440c7e21ebc23778a89327
ForgottenDead <chrishalo3king@hotmail.com>
2018-05-07 08:16:14 -0400

    starting login gui server                --done but idk if working client                --need to finish        Get color      --make the color decided by sender        Only kill enemy pieces Send commands over tcp GUI                    --may be done need to test      Sign up           --need to add functionality      List of users     --ned to get working Timer                  --need to add
    Castling
    Promotion
    Check

--- src/Client/Server/Server.java
+++ src/Client/Server/Server.java
@@ -1,73 +1,67 @@
 import java.io.*;
 import java.net.*;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 class Server {
     public static void main(String argv[]) throws Exception {
         String receivedMessage;
         ServerSocket serverSocket = new ServerSocket(6789);
-        HashMap<String, Socket> loggedInUsers = new HashMap<String, Socket>();
+        HashMap<String, Socket> loggedInUsers = new HashMap<>();
+        HashMap<String, String> joinedUsers = new HashMap<>();
         String[] message;
         String command;
+        String color;
+        String requestee;
+        String requesting;
+        String receiver;
         while (true) {
             Socket socket = serverSocket.accept();
             BufferedReader inFromClient =
                     new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
             receivedMessage = inFromClient.readLine();
             message=receivedMessage.substring(2).split(" ");
             command=message[0];
+
+            //DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
+            DataOutputStream outToClient;
             if(command.equalsIgnoreCase("new")){
                 loggedInUsers.put(message[1], socket);
-                outToClient.writeBytes("--names " + loggedInUsers.keySet().toArray() + "\n");
+
+                Iterator it = loggedInUsers.entrySet().iterator();
+                while(it.hasNext()){
+                    Map.Entry pair = (Map.Entry)it.next();
+                    outToClient = new DataOutputStream(((Socket)pair.getValue()).getOutputStream());
+                    outToClient.writeBytes("--names " + loggedInUsers.keySet().toArray() + "\n");
+                    it.remove();
+                }
             }
-            else if(receivedMessage.substring(2).split(" ").toString()
-                    .equalsIgnoreCase("join")){
-                outToClient.writeBytes("--request " + message[1] + "\n");
+            else if(command.equalsIgnoreCase("join")){
+                requestee=message[1];
+                requesting=message[2];
+                outToClient = new DataOutputStream(loggedInUsers.get(requestee).getOutputStream());
+                outToClient.writeBytes("--request " + requesting + "\n");
             }
             else if(command.equalsIgnoreCase("accept")){
-                outToClient.writeBytes("--accept " + userColor + "\n");
+                requestee=message[1];
+                requesting=message[2];
+                joinedUsers.put(requesting, requestee);
+                color=message[3];
+                outToClient = new DataOutputStream(loggedInUsers.get(requestee).getOutputStream());
+                outToClient.writeBytes("--accept " + color + "\n");
             }
             else if(command.equalsIgnoreCase("reject")){
+                requestee=message[1];
+                requesting=message[2];
+                outToClient = new DataOutputStream(loggedInUsers.get(requestee).getOutputStream());
                 outToClient.writeBytes("--reject\n");
             }
             else if(command.equalsIgnoreCase("move")){
-                outToClient.writeBytes("--move " + command[2] + " " + command[3] + " " + command[4] + "\n");
+                receiver=joinedUsers.get(message[1]);
+                outToClient = new DataOutputStream(loggedInUsers.get(receiver).getOutputStream());
+                outToClient.writeBytes("--move " + message[2] + " " + message[3] + " " + message[4] + "\n");
             }
         }
     }
-    public static void addUser(){
-        //add user
-    }
-    public static void request(){
-        //request to join
-        //requestPlay.writeBytes("--request " + command[1] + "\n");
-
-    }
-    public static void accept(){
-        //accept
-        //acceptPlay.writeBytes("--accept " + userColor + "\n");
-    }
-    public static void reject(){
-        //acceptPlay.writeBytes("--reject\n");
-    }
-    public static void move(){
-        //move piece
-        //sendMove.writeBytes("--move " + command[2] + " " + command[3] + " " + command[4] + "\n");
-        //command[2] = the piece being moved,
-        // command[3] = the x coordinate.
-        // Command[4] is the y coordinates
-    }
-    public static void sendList(){
-        //send to anyone connected
-        //sender.writeBytes("--names " + names + "\n");
-        // Sends the usernames of all those connected to the client
-    }
-//    public static void RemoveUsers(){
-//
-//    }
-//    public static void exit(){
-//
-//    }
-
 }
